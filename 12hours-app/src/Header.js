448d244e8060914900886e0d93aa212884b06bd3
PhanPhuocDat <1824801030046@tdmu.edu.vn>
2020-12-23 16:13:44 +0700

    up project

---
+++ 12hours-app/src/Header.js
@@ -0,0 +1,15 @@
+/*rfce */
+import React from 'react' ;
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+
+function Header() {
+    return (
+        <div className="header">
+            <h2> too la header</h2>
+            <PersonIcon fontSize=""/>
+        </div>
+    )
+}
+
+export default Header
